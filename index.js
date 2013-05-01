835cfcfa85a022150dc54a5d083f40f334cf84de
Noah Isaacson <noah@nisaacson.com>
2013-05-01 00:23:28 -0400

    added bind when mixing in functions

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function(base, extendMe) {
   var prop
   for (prop in base) {
     if (typeof base[prop] === 'function' && !extendMe[prop]) {
-      extendMe[prop] = base[prop]
+      extendMe[prop] = base[prop].bind(extendMe)
     }
   }
 }

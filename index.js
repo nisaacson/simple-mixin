07b30f8baba4e91f8e66bdd4bf287b699b7dec2a
Noah Isaacson <noah@nisaacson.com>
2013-05-01 00:32:55 -0400

    fix bind param

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function(base, extendMe) {
   var prop
   for (prop in base) {
     if (typeof base[prop] === 'function' && !extendMe[prop]) {
-      extendMe[prop] = base[prop].bind(extendMe)
+      extendMe[prop] = base[prop].bind(base)
     }
   }
 }
